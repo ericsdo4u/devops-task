21fa3e6c2efd02d0c8ed58a6d8f38e3f8cf8684d
ericsdo4u <ericsonericdon6@gmail.com>
2024-09-05 10:24:33 +0100

    hello-world

--- src/main/java/com/semicolon/devopsassignment/hello/HelloWorld.java
+++ src/main/java/com/semicolon/devopsassignment/hello/HelloWorld.java
@@ -1,2 +1,13 @@
-package com.semicolon.devopsassignment.hello;public class HelloWorld {
+package com.semicolon.devopsassignment.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/HELLO WORLD")
+    public String sayHelloWorld(){
+        return "hello world";
+    }
 }
